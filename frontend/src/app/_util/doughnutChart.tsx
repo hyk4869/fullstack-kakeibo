e9bbc9cd00807b8aa861aef82d7e2624f0474c3b
hyk4869 <hyk4869@gmail.com>
2023-11-26 18:23:54 +0900

    ドーナツグラフ途中

---
+++ frontend/src/app/_util/doughnutChart.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+type DoughnutChartProps = {
+  //
+};
+
+const DoughnutChart: React.FC<DoughnutChartProps> = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
