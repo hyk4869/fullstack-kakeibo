b2a66558697303e01965b979635038174f6c5866
hyk4869 <hyk4869@gmail.com>
2023-11-26 18:24:41 +0900

    集計っぽい画面作成

--- frontend/src/app/main/monthlyAggregation/monthlyAggregationPage.tsx
+++ frontend/src/app/main/monthlyAggregation/monthlyAggregationPage.tsx
@@ -1,23 +1,40 @@
 'use client';
-import { RootState } from '@/app/_store/store';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { Box, Paper, Tab } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import React, { useState } from 'react';
+import AggregationByCategory from './aggregationByCategory';
 
 type MonthlyAggregationProps = {
   //
 };
 const MonthlyAggregation: React.FC<MonthlyAggregationProps> = () => {
-  const monthlyData = useSelector((state: RootState) => state.getMonthlySpendingContent);
+  const [changePage, setChangePage] = useState<string>('1');
+
+  const hamdleChangePage = (e: React.SyntheticEvent, newValue: string) => {
+    setChangePage(newValue);
+  };
 
   return (
     <>
-      <div>
-        {monthlyData.map((a) => {
-          return <div key={a.id}>{a.id}</div>;
-        })}
-      </div>
+      <Box sx={{ width: '100%' }}>
+        <Paper sx={{ width: '95%', margin: '1rem auto', background: grey[50] }}>
+          <TabContext value={changePage}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={hamdleChangePage}>
+                <Tab label="カテゴリーごとの集計" value="1" />
+                <Tab label="全期間の合計" value="2" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              <AggregationByCategory />
+            </TabPanel>
+            <TabPanel value="2">item two</TabPanel>
+          </TabContext>
+        </Paper>
+      </Box>
     </>
   );
 };
 
-export default MonthlyAggregation;
+export default React.memo(MonthlyAggregation);
