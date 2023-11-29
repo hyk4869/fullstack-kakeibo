61d627909d13fa3f5f4aca052dbf3e6b4435a82d
hyk4869 <hyk4869@gmail.com>
2023-11-29 22:50:23 +0900

    ラジオボタン下書き

--- frontend/src/app/_customComponents/customRadioButton.tsx
+++ frontend/src/app/_customComponents/customRadioButton.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import React, { useState } from 'react';
+
+export type valueObjType = {
+  label: string;
+  value: string;
+};
 
 type CustomRadioButtonProps = {
   edit: boolean;
-  falseText: string;
-  trueText: string;
-  value: string;
+  valueObj: valueObjType[];
   paramkey?: string;
   id?: number;
   onChangeValue?: () => void;
@@ -12,11 +16,32 @@ type CustomRadioButtonProps = {
 };
 
 const CustomRadioButton: React.FC<CustomRadioButtonProps> = (props) => {
-  const { edit, falseText, trueText, value, paramkey, id, onChangeValue, defaultValue } = props;
+  const { edit, valueObj, paramkey, id, onChangeValue, defaultValue } = props;
+  const [radioValue, setRadioValue] = useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRadioValue(event.target.value);
+  };
+
   return (
-    <>
-      <div></div>
-    </>
+    <Box>
+      <FormControl>
+        <RadioGroup>
+          {valueObj.map((a) => {
+            return (
+              <>
+                <FormControlLabel
+                  value={a.value}
+                  label={a.label}
+                  control={<Radio onChange={(e) => handleChange(e)} />}
+                  disabled={!edit}
+                />
+              </>
+            );
+          })}
+        </RadioGroup>
+      </FormControl>
+    </Box>
   );
 };
 
