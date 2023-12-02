4be74f244fa32ed08ee4bde09532ff0e0e497a35
hyk4869 <hyk4869@gmail.com>
2023-12-02 23:17:52 +0900

    カテゴリーテーブルのスタイル調整

--- frontend/src/app/main/category/categoyTable.tsx
+++ frontend/src/app/main/category/categoyTable.tsx
@@ -1,13 +1,13 @@
 'use client';
 import { RootState } from '@/app/_store/store';
-import { Box, Paper, Table, TableCell, TableContainer, TableRow } from '@mui/material';
+import { Box, Paper, Table, TableCell, TableContainer, TableRow, TableHead, TableBody } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import CustomNumberFormat from '../../_customComponents/customNumeric';
 import CustomTextfield from '../../_customComponents/customTextfield';
 import RedirectDialog from '@/app/_util/redirectDialog';
-import { messageRedirect } from '@/app/_customComponents/customProperties';
+import { messageRedirect, commonPadding5 } from '@/app/_customComponents/customProperties';
 
 type CategoryTableProps = {
   //
@@ -56,41 +56,44 @@ const CategoryTable: React.FC<CategoryTableProps> = () => {
           <Box></Box>
           <TableContainer>
             <Table>
-              <TableRow>
-                {categoryHeader.map((a) => {
-                  return <TableCell key={a.value}>{a.label}</TableCell>;
+              <TableHead>
+                <TableRow sx={{ padding: commonPadding5 }}>
+                  {categoryHeader.map((a) => {
+                    return (
+                      <TableCell key={a.value} align={'center'} sx={{ padding: commonPadding5 }}>
+                        {a.label}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {categoryData.map((a) => {
+                  return (
+                    <TableRow key={a.categoryId} sx={{ padding: commonPadding5 }}>
+                      <TableCell align="center" sx={{ padding: commonPadding5 }}>
+                        <CustomNumberFormat
+                          value={a.categoryId}
+                          edit={false}
+                          align="center"
+                          onChangeValue={changeValue}
+                          paramKey={'id'}
+                          id={Number(a.categoryId)}
+                        />
+                      </TableCell>
+                      <TableCell align="center" sx={{ padding: commonPadding5 }}>
+                        <CustomTextfield
+                          value={a.categoryName}
+                          edit={edit}
+                          onChangeValue={changeValue}
+                          paramKey={'store'}
+                          id={Number(a.categoryId)}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  );
                 })}
-              </TableRow>
-            </Table>
-          </TableContainer>
-
-          <TableContainer>
-            <Table>
-              {categoryData.map((a) => {
-                return (
-                  <TableRow key={a.categoryId}>
-                    <TableCell>
-                      <CustomNumberFormat
-                        value={a.categoryId}
-                        edit={false}
-                        align="center"
-                        onChangeValue={changeValue}
-                        paramKey={'id'}
-                        id={Number(a.categoryId)}
-                      />
-                    </TableCell>
-                    <TableCell>
-                      <CustomTextfield
-                        value={a.categoryName}
-                        edit={edit}
-                        onChangeValue={changeValue}
-                        paramKey={'store'}
-                        id={Number(a.categoryId)}
-                      />
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
+              </TableBody>
             </Table>
           </TableContainer>
         </Paper>
