257e8f69999a03db6e0f5eeba7eee767efda7c1d
hyk4869 <hyk4869@gmail.com>
2023-12-06 20:50:09 +0900

    エンドポイント追加

--- frontend/src/app/_api/url.ts
+++ frontend/src/app/_api/url.ts
@@ -1,4 +1,4 @@
-/** 全期間のmonthlySpendingを取得 */
+/** 全期間のmonthlySpendingを取得&データのPOSTも行っている */
 export const getMonthlySpending = 'http://localhost:3005/summaryTable';
 /** 全期間のcategoryを取得 */
 export const getCategory = 'http://localhost:3005/summaryTable/category';
@@ -6,3 +6,7 @@ export const getCategory = 'http://localhost:3005/summaryTable/category';
 export const getSomeMonthlySpending = 'http://localhost:3005/summaryTable/someContent';
 /** 削除用の POST */
 export const postDeleteMonthlySpending = 'http://localhost:3005/summaryTable/deleteContent';
+/** 会社情報を取ってくる */
+export const getCompany = 'http://localhost:3005/salaryInfo/companyName';
+/** 入退社情報を取ってくる */
+export const getHireDate = 'http://localhost:3005/salaryInfo/hireDate';
