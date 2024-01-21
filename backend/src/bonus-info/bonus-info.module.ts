7b7c8263f3242898ab3493c21a4e8217c9deab49
hyk4869 <hyk4869@gmail.com>
2024-01-21 20:46:28 +0900

    バックエンド側でのエラー解消

--- backend/src/bonus-info/bonus-info.module.ts
+++ backend/src/bonus-info/bonus-info.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BonusInfoController } from './bonus-info.controller';
 import { BonusInfoService } from './bonus-info.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BonusInfoController],
-  providers: [BonusInfoService]
+  providers: [BonusInfoService, PrismaService],
 })
 export class BonusInfoModule {}
