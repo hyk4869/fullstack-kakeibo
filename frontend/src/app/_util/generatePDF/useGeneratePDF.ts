0e1ada03761fab71e88174577ac050dcd79f01e6
hyk4869 <hyk4869@gmail.com>
2024-01-28 19:56:12 +0900

    PDF生成のサンプルを作成

---
+++ frontend/src/app/_util/generatePDF/useGeneratePDF.ts
@@ -0,0 +1,46 @@
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import type { CustomTableLayout, TDocumentDefinitions } from 'pdfmake/interfaces';
+
+const fonts = {
+  NotoSerifJP: {
+    normal: 'https://fonts.gstatic.com/s/notoserifjp/v21/xn7mYHs72GKoTvER4Gn3b5eMXNikYkY0T84.otf',
+    bold: 'https://fonts.gstatic.com/s/notoserifjp/v21/xn77YHs72GKoTvER4Gn3b5eMZGKLRkgfU8fEwb0.otf',
+    italics: 'https://fonts.gstatic.com/s/notoserifjp/v21/xn7mYHs72GKoTvER4Gn3b5eMXNikYkY0T84.otf',
+    bolditalics: 'https://fonts.gstatic.com/s/notoserifjp/v21/xn77YHs72GKoTvER4Gn3b5eMZGKLRkgfU8fEwb0.otf',
+  },
+};
+
+/**
+ * PDFを作成するためのutil
+ * @returns
+ */
+export const useGeneratePDF = () => {
+  /** 同じページでPDFを開く */
+  const createPDF = async (
+    setPdfDataUrl: React.Dispatch<React.SetStateAction<string>>,
+    definition: TDocumentDefinitions,
+    tableLayouts?: { [name: string]: CustomTableLayout },
+  ) => {
+    return new Promise((resolve) => {
+      pdfMake.createPdf(definition, tableLayouts, fonts, pdfFonts.pdfMake?.vfs).getDataUrl((value) => {
+        setPdfDataUrl(value);
+        resolve(value);
+      });
+    });
+  };
+
+  /** 別ページでPDFを開く */
+  const createOpenPDF = (definition: TDocumentDefinitions, tableLayouts?: { [name: string]: CustomTableLayout }) => {
+    return new Promise(() => {
+      pdfMake.createPdf(definition, tableLayouts, fonts, pdfFonts.pdfMake?.vfs).open();
+    });
+  };
+
+  return {
+    createPDF,
+    createOpenPDF,
+  };
+};
+
+// frontend\src\app\main\test\TestPDF.tsx ここに使い方のサンプルがある
